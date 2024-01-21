00037b58375e7e79b7825f7b3cc581c1b7b8c41b
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-01-21 12:57:42 -0300

    feat: restart total

---
+++ src/deps/remote.libs_node.ts
@@ -0,0 +1,10 @@
+import * as path from "remotes";
+
+const _dirname: any = path.dirname(path.fromFileUrl(import.meta.url))
+
+
+
+export {
+  path,
+  _dirname,
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { entityOne } from "entitys";
+import { _dirname } from 'use_libs'
+
 
 // console.log("Hello Word")
+
+console.log(`LIB DIRNAME >> ${_dirname} === ACABOU_LIB.`)
+
+// console.log(`Usando Deno.cwd() esta voltando : ${Deno.cwd()} --- final.`)
 
 console.log(entityOne())
